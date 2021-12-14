533a31531c98ed7b37a4aeecb37df6633b6aee69
Duncan Campbell <duncan.campbell@epistemix.com>
2021-12-14 17:16:48 -0500

    added version meta data

--- epxresults/__init__.py
+++ epxresults/__init__.py
@@ -8,3 +8,20 @@ from .utils import *
 from .job import *
 from .run import *
 from .insert import *
+
+
+import os
+from pathlib import Path
+import pytest
+
+# version
+mypackage_root_dir = Path(os.path.dirname(os.path.abspath(__file__))).parent
+with open(os.path.join(mypackage_root_dir, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+
+
+def test():
+    """
+    run pytest tests
+    """
+    retcode = pytest.main()
